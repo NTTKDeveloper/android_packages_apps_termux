2a3f506e3655871966f3d92a66fe1e0ef4576070
NTTKDeveloper <nttkct2020@gmail.com>
2023-11-08 09:39:13 +0700

    first commit

---
+++ Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE := termux
+
+LOCAL_SRC_FILES := termux
+
+LOCAL_MODULE_CLASS := APPS
+
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+
+include $(TARGET_OUT_DATA)
